e5877a38185fcca73bdaadee05ce4dc06213745e
amartinhit2 <amartinhit2@educacion.navarra.es>
2025-12-17 10:26:27 +0100

    Constructor añadido

--- Examen_Alejandro/src/Examen/Libro.java
+++ Examen_Alejandro/src/Examen/Libro.java
@@ -1,5 +1,18 @@
 package Examen;
 
 public class Libro {
-
+	String titulo;
+	String autor;
+	String isbn;
+	boolean prestado;
+	
+	public Libro(String titulo, String autor, String isbn, boolean prestado) {
+		super();
+		this.titulo = titulo;
+		this.autor = autor;
+		this.isbn = isbn;
+		this.prestado = prestado;
+	}
+	
+	
 }
